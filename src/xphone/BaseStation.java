ad3d7998ce885082c3b8ec5a062541ec2eb9052f
Stefan Åhman <stefan.ahman@gmail.com>
2011-10-03 15:59:26 +0200

    Fixed BaseStations

--- src/xphone/BaseStation.java
+++ src/xphone/BaseStation.java
@@ -3,14 +3,16 @@ package xphone;
 public class BaseStation {
 
 	int id;
-	int channels;
-	int reservedChannels;
-	int currentNumberOfCalls;
-	int radius;
+	int channels = 0;
+	int reservedChannels = 0;
+	int currentUsedChannels = 0;
+	int currentUsedReservedChannels = 0;
+	int radius = 0;
 	
-	BaseStation(int id, int channels, int radius){
+	BaseStation(int id, int channels, int reservedChannels, int radius){
 		this.id = id;
 		this.channels = channels;
+		this.reservedChannels = reservedChannels;
 		this.radius = radius;
 	}
 	
@@ -26,21 +28,36 @@ public class BaseStation {
 		return reservedChannels;
 	}
 	
-	public void setReservedChannels(int reservedChannels) {
-		this.reservedChannels = reservedChannels;
-	}
-	
 	public int getCurrentNumberOfCalls() {
-		return currentNumberOfCalls;
+		return currentUsedChannels;
 	}
 	
-	public void setCurrentNumberOfCalls(int currentNumberOfCalls) {
-		this.currentNumberOfCalls = currentNumberOfCalls;
+	public void setCurrentNumberOfCalls(int currentUsedChannels) {
+		this.currentUsedChannels = currentUsedChannels;
 	}
 	
 	public int getRadius() {
 		return radius;
 	}
 	
+	public int getCurrentUsedReservedChannels() {
+		return currentUsedReservedChannels;
+	}
+
+	public void setCurrentUsedReservedChannels(int currentUsedReservedChannels) {
+		this.currentUsedReservedChannels = currentUsedReservedChannels;
+	}
+
+	public void setReservedChannels(int reservedChannels) {
+		this.reservedChannels = reservedChannels;
+	}
+
+	public boolean isFull() {
+		return currentUsedChannels >= (channels - reservedChannels);
+	}
+	
+	public boolean isReservationAvailable() {
+		return currentUsedReservedChannels < reservedChannels;
+	}
 	
 }
